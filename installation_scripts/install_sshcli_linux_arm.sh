8c1f659ce14cb1e6b90e4029defd63efd9f16c97
Naseredin Aramnejad <naseredin.aramnejad@nokia.com>
2025-09-03 18:04:37 +0200

    update installation scripts

---
+++ installation_scripts/install_sshcli_linux_arm.sh
@@ -0,0 +1,30 @@
+GITLAB_URL="https://github.com/naseriax/sshcli/releases/download/20250903.0836/sshcli_linux_arm64"
+DEST_DIR="$HOME/sshcli"
+NEW_NAME="sshcli"
+echo "Starting download of the sshcli executable for Linux (ARM64)..."
+echo "Creating directory: $DEST_DIR"
+mkdir -p "$DEST_DIR" || { echo "Error: Failed to create directory. Exiting."; exit 1; }
+FINAL_PATH="$DEST_DIR/$NEW_NAME"
+echo "Downloading file from: $GITLAB_URL"
+curl -L --progress-bar "$GITLAB_URL" --output "$FINAL_PATH"
+if [ $? -ne 0 ]; then
+    echo "Error: curl download failed."
+    exit 1
+fi
+echo "Download complete! The executable has been saved to:"
+echo "$FINAL_PATH"
+echo "Setting executable permissions..."
+chmod +x "$FINAL_PATH"
+echo "Adding sshcli to PATH"
+echo "# Below line was added by sshcli" >> ~/.bashrc
+echo 'export PATH="$HOME/sshcli:$PATH"' >> ~/.bashrc
+echo ""
+echo "🎉 Success! sshcli has been installed."
+echo ""
+echo "To finish the setup, please do one of the following:"
+echo "1) Open a new terminal window."
+echo "   OR"
+echo "2) Run the following command in your current terminal:"
+echo "   source ~/.bashrc"
+echo ""
+exit 0
--- installation_scripts/install_sshcli_mac_x86_64.sh
+++ installation_scripts/install_sshcli_mac_x86_64.sh
@@ -1,43 +1,20 @@
-
-
-# Full URL to the RAW executable file on GitLab for macOS ARM64.
-GITLAB_URL="https://github.com/naseriax/sshcli/releases/download/20250903.0836/sshcli_mac_arm64"
-
-# The destination directory within the user's home folder.
+GITLAB_URL="https://github.com/naseriax/sshcli/releases/download/20250903.0836/sshcli_mac_amd64"
 DEST_DIR="$HOME/sshcli"
-
-# The new name for the executable after it is downloaded.
 NEW_NAME="sshcli"
-
-# ==============================================================================
-# SCRIPT LOGIC (DO NOT EDIT BELOW THIS LINE)
-# ==============================================================================
-
-echo "Starting download of the sshcli executable for macOS (ARM64)..."
-
-# Create the destination directory
+echo "Starting download of the sshcli executable for macOS (AMD64)..."
 echo "Creating directory: $DEST_DIR"
 mkdir -p "$DEST_DIR" || { echo "Error: Failed to create directory. Exiting."; exit 1; }
-
-# The final path for the executable.
 FINAL_PATH="$DEST_DIR/$NEW_NAME"
-
-# Download the file
 echo "Downloading file from: $GITLAB_URL"
 curl -L --progress-bar "$GITLAB_URL" --output "$FINAL_PATH"
 if [ $? -ne 0 ]; then
     echo "Error: curl download failed."
     exit 1
 fi
-
-
 echo "Download complete! The executable has been saved to:"
 echo "$FINAL_PATH"
-
-# Give the file execute permissions
 echo "Setting executable permissions..."
 chmod +x "$FINAL_PATH"
-
 echo "Adding sshcli to PATH"
 echo "# Below line was added by sshcli" >> ~/.zshrc
 echo 'export PATH="$HOME/sshcli:$PATH"' >> ~/.zshrc
@@ -50,5 +27,4 @@ echo "   OR"
 echo "2) Run the following command in your current terminal:"
 echo "   source ~/.zshrc"
 echo ""
-
 exit 0
